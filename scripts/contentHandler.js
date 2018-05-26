57b10a3c41cd20cebc112a86136f6c1ed6ce1f33
Drew Borkowski <ajborkowski1031@gmail.com>
2018-05-26 16:00:45 -0400

    Proto setup

--- scripts/contentHandler.js
+++ scripts/contentHandler.js
@@ -1,18 +1,28 @@
-var view;
-//avar content = "TEST <li>List Item</li>";
-
-function loadContent(section) {
-    THREE.Cache.enabled = true;
-
-    var plane = document.getElementById("info");
-
+function loadContent() {
+    
+    THREE.Cache.enabled = true;    
     var loader = new THREE.FileLoader();
-
     loader.load(
-        "../html/" + section + ".html",
+        "../html/bio.txt",
         function(data) {
-            plane.setAttribute("text", {value: data, color: "red"});
-            //console.log(data);
+            var bio = document.getElementById("bio");
+            bio.setAttribute("text", {value: data});
         }
     );
+    loader.load(
+        "../html/resume.txt",
+        function(data) {
+            var resume = document.getElementById("resume");
+            resume.setAttribute("text", {value: data});
+        }
+    );
+    loader.load(
+        "../html/projects.html",
+        function(data) {
+            var projects = document.getElementById("projects");
+            projects.setAttribute("text", {value: data});
+        }
+    );
+
 }
+
