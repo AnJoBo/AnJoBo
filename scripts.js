9c5a2d29ddbab3179de4b781293d53b4eca8d5b3
ajborkowski <ajborkowski1031@gmail.com>
2018-07-04 00:26:08 -0400

    adding interactivity with js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function hover(element) {
+    element.setAttribute('src', 'me00-1.png');
+  }
+  
+function unhover(element) {
+element.setAttribute('src', 'me00.png');
+}
+
+//TODO: update shadows on mouse location
