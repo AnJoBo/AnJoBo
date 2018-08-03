4250c1328c10218432f65fc4787418eca01f411f
ajborkowski <ajborkowski1031@gmail.com>
2018-08-02 21:19:06 -0400

    disabled A-Frame scene on screens smaller than 1200px in width, fixed logo rotation, minor changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 
+    // Checks if window is big enough for A-Frame scene
+    if($(window).width() >= 1200){
+        $("#head-canvas").load("scene.html");
+    } else {
+        $("#head-canvas").css("background-color", "rgba(32, 32, 32, 0.5)");
+    }
+    
+
     // ---- Form Events ---- 
     $("input").focus(function(){
         $(this).css("background-color", "#fff");
@@ -8,26 +16,6 @@ $(document).ready(function(){
     $("input").blur(function(){
         $(this).css("background-color", "rgb(201, 201, 201)");
     });
-    
-    //---- Scroll Animation ---
-    function scrollNav() {
-        $('.nav a').click(function(){  
-          //Toggle Class
-          $(".active").removeClass("active");      
-          $(this).closest('li').addClass("active");
-          var theClass = $(this).attr("class");
-          $('.'+theClass).parent('li').addClass('active');
-          //Animate
-          $('html, body').stop().animate({
-              scrollTop: $( $(this).attr('href') ).offset().top - 160
-          }, 400);
-          return false;
-        });
-        $('.scrollTop a').scrollTop();
-      }
-      scrollNav();
-
-    
 
 });
 
